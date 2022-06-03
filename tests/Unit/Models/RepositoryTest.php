a5c86fe947da8bf8929ca6dfc3d9497c166242ef
Orangel Barrera <obarrerafranco@gmail.com>
2022-06-03 10:34:40 +0200

    New models and tests for Users and Repositories

---
+++ tests/Unit/Models/RepositoryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+use App\Models\Repository;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_belongs_to_user()
+    {
+        $repository = Repository::factory()->create();
+
+        $this->assertInstanceOf(User::class,$repository->user);
+    }
+}
