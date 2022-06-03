1543a1a3b4e3134e4a77ad25ce245a1cc43fd5cc
Orangel Barrera <obarrerafranco@gmail.com>
2022-06-03 14:50:34 +0200

    Create repository store Test

--- app/Models/Repository.php
+++ app/Models/Repository.php
@@ -9,6 +9,11 @@ class Repository extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'url',
+        'description'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -5,14 +5,11 @@ namespace Tests\Feature\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class RepositoryControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use WithFaker, RefreshDatabase;
     public function test_guest()
     {
         $this->get('repositories')->assertRedirect('login');        // index
@@ -22,5 +19,23 @@ class RepositoryControllerTest extends TestCase
         $this->delete('repositories/1')->assertRedirect('login');   // destroy
         $this->get('repositories/create')->assertRedirect('login'); // create
         $this->post('repositories', [])->assertRedirect('login');   // store
+    }
+
+    public function test_store()
+    {
+        $data = [
+            'url' => $this->faker->url,
+            'description' => $this->faker->text,
+        ];
+
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->post('repositories', $data)
+            ->assertRedirect('repositories');
+
+        $this->assertDatabaseHas('repositories', $data);
+
     }
 }
